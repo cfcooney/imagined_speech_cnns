f9e267951cda1c72313c59b1d84e23249ddacf25
Ciaran Cooney <35545610+cfcooney@users.noreply.github.com>
2019-01-15 08:34:07 +0000

    Update utils.py
    Added print_confusion_matrix function.

--- cnns/utils.py
+++ cnns/utils.py
@@ -188,3 +188,58 @@ def return_indices(event_id, labels):
                 idx.append(d)
         indices.append(idx)
     return indices
+
+"""
+Confusion matrix plotting using Seaborn. Matplotlib below.
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def print_confusion_matrix(confusion_matrix, class_names, filename, normalize = True, figsize = (5,5), fontsize=16):
+    """Prints a confusion matrix, as returned by sklearn.metrics.confusion_matrix, as a heatmap.
+    
+    Arguments
+    ---------
+    confusion_matrix: numpy.ndarray
+        The numpy.ndarray object returned from a call to sklearn.metrics.confusion_matrix. 
+        Similarly constructed ndarrays can also be used.
+    class_names: list
+        An ordered list of class names, in the order they index the given confusion matrix.
+    figsize: tuple
+        A 2-long tuple, the first value determining the horizontal size of the ouputted figure,
+        the second determining the vertical size. Defaults to (10,7).
+    fontsize: int
+        Font size for axes labels. Defaults to 14.
+        
+    Returns
+    -------
+    matplotlib.figure.Figure
+        The resulting confusion matrix figure
+    """
+    if normalize:
+        confusion_matrix = (confusion_matrix.astype('float') / confusion_matrix.sum(axis=1)[:, np.newaxis])*100
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    
+    df_cm = pd.DataFrame(
+        confusion_matrix, index=class_names, columns=class_names, 
+    )
+    fig = plt.figure(figsize=figsize)
+    fmt = '.2f' if normalize else 'd'
+    #####set heatmap customization#####
+    try:
+        heatmap = sns.heatmap(df_cm, annot=True, fmt=fmt, cmap='GnBu', linewidths=.5, cbar=False, annot_kws={"size": 16})
+    except ValueError:
+        raise ValueError("Confusion matrix values must be integers.")
+        
+    heatmap.yaxis.set_ticklabels(heatmap.yaxis.get_ticklabels(), rotation=0, ha='right', fontsize=fontsize)
+    heatmap.xaxis.set_ticklabels(heatmap.xaxis.get_ticklabels(), rotation=45, ha='right', fontsize=fontsize)
+    plt.ylabel('True label', fontsize=16, fontname='sans-serif')
+    plt.xlabel('Predicted label', fontsize=16, fontname='sans-serif')
+    
+    if filename != None:
+        fig.savefig(filename + '.png', bbox_inches='tight') #store image as .png
+    
+    return fig
